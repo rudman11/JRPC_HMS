558579ed3f5ed5fe90efbe98f05bdbc041a0df85
rudman11 <rudman11@gmail.com>
2020-03-16 06:52:01 +0200

    Add files via upload

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,108 @@
+﻿using JRPC_HMS.Data;
+using JRPC_HMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JRPC_HMS.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrder(int id)
+        {
+            var order = _context.Orders.Find(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public IActionResult PutOrder(int id, [FromBody]Order order)
+        {
+            Order orderr = new Order();
+            orderr = _context.Orders.Find(id);
+            if (id != orderr.Id)
+            {
+                return BadRequest();
+            }
+            orderr.PreparationStatus = "Complete";
+            _context.Entry(orderr).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Order>> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return order;
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/SettingsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JRPC_HMS.Controllers
+{
+    [Authorize]
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToPage("/Settings/Profile");
+        }
+
+        public IActionResult TwoFactorAuth()
+        {
+            return RedirectToPage("/Settings/TwoFactorAuth/Config");
+        }
+    }
+}
